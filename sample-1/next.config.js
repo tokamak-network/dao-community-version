40e5fd62492755e9652e60c8d7c56c42c42910a1
Zena-park <zena@tokamak.network>
2025-07-31 18:45:20 +0900

    Update next.config.js

--- sample-1/next.config.js
+++ sample-1/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/home',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
